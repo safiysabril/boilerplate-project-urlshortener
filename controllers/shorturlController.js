a32315357fb90cc2fe783b01a5287c3c3f045b0c
Muhammad Safiy <safiysabril@gmail.com>
2024-06-24 22:47:42 +0800

    add error handling

--- controllers/shorturlController.js
+++ controllers/shorturlController.js
@@ -11,8 +11,11 @@ const createShortUrl = (req, res) => {
     let parsedUrl;
     try {
         parsedUrl = new URL(userUrl);
+        if (parsedUrl.protocol !== 'http:' && parsedUrl.protocol !== 'https:') {
+            throw new Error('Invalid URL');
+        }
     } catch (err) {
-        return res.status(400).json({ error: "Invalid URL" });
+        return res.json({ error: "Invalid URL" });
     }
 
     // Verify URL using dns.lookup
@@ -30,6 +33,17 @@ const createShortUrl = (req, res) => {
             urlId++;
         }
     });
-}
+};
 
-module.exports = { createShortUrl };
+const redirectShortUrl = (req, res) => {
+    const shortUrlId = req.params.short_url;
+    const originalUrl = urlDatabase[shortUrlId];
+
+    if (originalUrl) {
+        res.redirect(originalUrl);
+    } else {
+        res.status(404).json({ error: 'No short URL found for the given input' });
+    }
+};
+
+module.exports = { createShortUrl, redirectShortUrl };
--- routers/shorturl.js
+++ routers/shorturl.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { createShortUrl } = require("../controllers/shorturlController");
+const { createShortUrl, redirectShortUrl } = require("../controllers/shorturlController");
 
 const router = Router();
 
 router.route("/shorturl").post(createShortUrl);
+router.route("/shorturl/:short_url").get(redirectShortUrl);
 
 module.exports = router;
